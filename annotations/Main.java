62c55c4c1b183b5dbdd1cdb96f12a74d26556eec
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-11-14 21:55:47 +0600

    Update annotation

--- annotations/Main.java
+++ annotations/Main.java
@@ -1,18 +1,56 @@
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-class A{
+// Ref: https://youtu.be/rWlHQnvrZcw
+/*
+
+annotation without any values -> Marker Annotation
+annotation with values -> Multi value annotation
+
+*/
+
+
+// Creating a annotation
+@Inherited // the derived classes will get inherit the annotation
+@Documented // create documentation of the annotation
+@Target(ElementType.TYPE) // TYPE -> class level
+@Retention(RetentionPolicy.RUNTIME) // meta annotation
+@interface SmartPhone 
+{   
+    String os() default "Symbian"; 
+    int version() default 1;
 
 }
 
-class B extends A {
-    @Override
-    @SuppressWarnings("unchecked")
-    @Deprecated
+@SmartPhone(os="Android", version = 6)
+class Nokia {
+    String model;
+    int size;
+
+
+
+    public Nokia(String model, int size) {
+        this.model = model;
+        this.size = size;
+    }
+
+
 }
 
 class Main{
     public static void main(String[] args) {
         
-        System.out.println("this is some line");
+        Nokia nokiaSix = new Nokia("6.1", 6);
+        
+        Class c = nokiaSix.getClass();
+        Annotation annotation = c.getAnnotation(SmartPhone.class);
+        SmartPhone s = (SmartPhone)annotation;
+        System.out.println(s.os());
 
     }
 }
