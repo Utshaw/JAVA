29b3ae2a54e0878cf2e5453e1d170a5a475fdcbc
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-11-18 09:08:54 +0600

    Remove type

--- enum/src/MyEnum.java
+++ enum/src/MyEnum.java
@@ -5,7 +5,7 @@ public class MyEnum {
     Why use enums instead of constants: https://goo.gl/54fdPH (for type safety)
     Using enum we can enforce type safety & we can add some features with the enum constants(using constructor & give value for the constants)
     Constructor of enum is always private so, new can't be used to create objects
-    All the constants declared in enum are statuc & final implicitly
+    All the constants declared in enum are static & final implicitly
     Though the name of the objects can't be changed but the value of associated with that object can be changed
     Actually  ,the constants are objects
     public static enum-type[] values() - return the array of Enum objects for Enumeration type for iteration
